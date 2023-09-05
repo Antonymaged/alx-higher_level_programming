35135b0538ad2a613ba83ab46c7c1e46b69c513c
Antonymaged <magedantony1@gmail.com>
2023-09-05 11:57:38 -0700

    ahggggggggghhhhhh

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in range(ord(a), ord(z)):
-        if c == chr(i):
-            return True
-        else:
-            return False
+    if ord(c) >= 97 and ord(c) <= 122:
+        return True
+    return False
