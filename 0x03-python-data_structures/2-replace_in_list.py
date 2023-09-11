3d51bfab5fc7083845f88ea5bf5e27320addd212
Antonymaged <magedantony1@gmail.com>
2023-09-11 20:06:49 +0000

    2-replace_in_list.py

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -8,5 +8,5 @@ def replace_in_list(my_list, idx, element):
     elif idx > leng:
         return (my_list)
     else:
-        mylist[idx] = element
+        my_list[idx] = element
         return (my_list)
