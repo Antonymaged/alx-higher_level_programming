0fd620b0487c171eb898ddd91dc02ec7077a26e2
Antonymaged <magedantony1@gmail.com>
2023-09-11 21:02:20 +0000

    4-new_in_list.py

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def new_in_list(my_list, idx, element):
+    leng = len(my_list)
+    new_list[] = my_list
+    if idx > leng or idx < 0:
+        return (new_list)
+    else:
+        new_list[idx] = element
+        return (new_list)
