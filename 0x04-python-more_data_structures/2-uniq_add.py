13e39df1c69c40cc91deb4815110fb8b478f17a9
Antonymaged <magedantony1@gmail.com>
2023-09-13 18:47:28 -0700

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,6 +4,7 @@
 def uniq_add(my_list=[]):
     new_lis = sorted(my_list)
     leng = len(new_lis)
+    sumy = 0
     for i in range(0, leng):
-        sum += new_lis[i]
-    return (sum)
+        sumy = sumy + new_lis[i]
+    return (sumy)
