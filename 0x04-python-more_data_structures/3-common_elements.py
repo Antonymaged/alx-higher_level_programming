f84c55b7a87f7e7932af726341829d99e7efae1e
Antonymaged <magedantony1@gmail.com>
2023-09-13 19:03:09 -0700

    3-common_elements.py

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -2,9 +2,4 @@
 
 
 def common_elements(set_1, set_2):
-    set_3 = []
-    for i in set_1:
-        for j in set_2:
-            if i == j:
-                set_3.append(i)
-    return(set_3)
+    return(set_1 & set_2)
