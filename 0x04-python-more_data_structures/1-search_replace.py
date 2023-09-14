8506895dd041da240612f70738325f5d2bc7eb09
Antonymaged <magedantony1@gmail.com>
2023-09-13 18:01:14 -0700

    1-search_replace.py

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def search_replace(my_list, search, replace):
+    for i in my_list
+        if search in my_list
+            my_list.index(search) = replace
+    return(my_list)
