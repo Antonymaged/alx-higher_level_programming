160a792adfb123c7529b2ddbe15dc2461765f362
Antonymaged <magedantony1@gmail.com>
2023-09-13 19:48:26 -0700

    12-roman_to_int.py

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
-def to_sub(listn):
-    tosub = 0
-    maxli = max(listn)
-    for n in listn:
-        if maxli > n:
-            tosub += n
-    return (maxli - tosub)
+def to_subtract(list_num):
+    to_sub = 0
+    max_list = max(list_num)
+
+    for n in list_num:
+        if max_list > n:
+            to_sub += n
+
+    return max_list - to_sub
+
+
 def roman_to_int(roman_string):
-    if not roman_string:
+    if not roman_string or not isinstance(roman_string, str):
         return 0
-    if not isinstance(roman_string, str):
-        return 0
-    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-    listk = list(rom.keys())
-    n = 0
-    lastrom = 0
-    listn = [0]
+
+    rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    list_keys = list(rom_n.keys())
+    num = 0
+    last_rom = 0
+    list_num = [0]
+
     for ch in roman_string:
-        for r_num in listk:
+        for r_num in list_keys:
             if r_num == ch:
-                if rom.get(ch) <= lastrom:
-                    n += to_sub(listn)
-                    listn = [rom.get(ch)]
+                if rom_n.get(ch) <= last_rom:
+                    num += to_subtract(list_num)
+                    list_num = [rom_n.get(ch)]
                 else:
-                    listn.append(rom.get(ch))
-                lastrom = rom.get(ch)
-    num += to_subtract(listn)
-    return (n)
+                    list_num.append(rom_n.get(ch))
+                last_rom = rom_n.get(ch)
+    num += to_subtract(list_num)
+    return num
