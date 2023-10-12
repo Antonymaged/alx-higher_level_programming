36026f921b5ef064aa70ba0ead4c29cba1882c02
Antonymaged <magedantony1@gmail.com>
2023-10-11 19:44:51 -0700

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Add all arguments to a Python list and save them to a file."""
+import sys
+import json
+
+def save_to_json_file(data, filename):
+    with open(filename, 'w') as file:
+        json.dump(data, file)
+
+def load_from_json_file(filename):
+    try:
+        with open(filename, 'r') as file:
+            data = json.load(file)
+            return data
+    except FileNotFoundError:
+        return []
+
+if __name__ == "__main__":
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
