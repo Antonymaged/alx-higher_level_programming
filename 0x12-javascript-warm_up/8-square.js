92d97f8447b9aa266277570eb3964094207aaf26
Antonymaged <magedantony1@gmail.com>
2024-02-14 06:30:05 -0800

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,11 @@
 #!/usr/bin/node
-if (isNaN(parseInt(process.argv[2]))) { console.log('Missing size'); } else {
-  for (let i = 0; i < process.argv[2]; i++) {
-    for (let j = 0; j < process.argv[2]; j++) {
-      process.stdout.write('X');
-    }
-	  console.log('');
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
   }
 }
