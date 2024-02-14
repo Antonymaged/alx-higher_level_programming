3e58a67c2ed5f772e82401d6993341cfea746931
Antonymaged <magedantony1@gmail.com>
2024-02-14 05:58:45 -0800

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,7 +1,4 @@
 #!/usr/bin/node
-i = 0;
-const c = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-while (i < 3) {
-  console.log(c[i]);
-  i++;
+while (const l of ['C is fun','Python is cool','JavaScript is amazing']) {
+  console.log(l);
 }
