0a42ec14a6dc7995e7c6266301d8dcd826fad3a3
Antonymaged <magedantony1@gmail.com>
2024-02-14 06:28:59 -0800

    8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,8 +2,8 @@
 if (isNaN(parseInt(process.argv[2]))) { console.log('Missing size'); } else {
   for (let i = 0; i < process.argv[2]; i++) {
     for (let j = 0; j < process.argv[2]; j++) {
-      process.stdout.write("X");
+      process.stdout.write('X');
     }
-	  console.log("");
+	  console.log('');
   }
 }
